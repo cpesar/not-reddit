5d21e24d39b4c9d0b1059bc27c79b9fd0959b6af
cpesar <cpesar1@gmail.com>
2025-02-16 18:41:05 -0700

    finished oauth setup and testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
 import { Button } from "@heroui/react";
+import * as actions from "@/lib/actions/auth.actions";
+import { auth } from "@/auth";
+import Profile from "@/components/ui/profile";
 
-const Home = () => {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
-      <Button>Click Me</Button>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign In</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Signed Out</div>
+      )}
+      <Profile />
     </div>
   );
-};
-
-export default Home;
+}
